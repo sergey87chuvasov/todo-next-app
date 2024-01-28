fb7905126d3ae46bd8f71176b717e6df00ebe7bf
Sergey87 <serge.techno@gmail.com>
2024-01-28 19:20:50 +0300

    added Send data on MongoDB database using Next.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 'use client';
 import Todo from '@/Components/Todo';
+import axios from 'axios';
 import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -24,8 +25,9 @@ export default function Home() {
     e.preventDefault();
     try {
       // api
+      const response = await axios.post('/api', formData);
 
-      toast.success('Success');
+      toast.success(response.data.msg);
     } catch (error) {
       toast.error('Error');
     }
